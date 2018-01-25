27aed29a277fdea5ff614cdc6032a2de336b1adc
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-25 02:33:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,7 @@ end
 def batch_badge_creator(name_array)
   badge_array = name_array.collect{|name| badge_maker(name)}
 end
+
+def assign_rooms(name_array)
+  room_assignments = name_array.collect{|name| "Hello, #{name}! You'll be assigned to room #{name_array.index(name) + 1}!"}
+end
