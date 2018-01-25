f50b9d3db0eb9740df56597703788048a324fd8a
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-25 02:41:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,13 +11,10 @@ def assign_rooms(name_array)
 end
 
 def printer(name_array)
-  #batch_badge_creator(name_array).each do |badge|
-  #  puts badge
-  #end
-  #assign_rooms(name_array).each do |assignment|
-  #  puts assignment
-  #end
-
-  puts batch_badge_creator(name_array)
-  puts assign_rooms(name_array)
+  batch_badge_creator(name_array).each do |badge|
+    puts badge
+  end
+  assign_rooms(name_array).each do |assignment|
+    puts assignment
+  end
 end
